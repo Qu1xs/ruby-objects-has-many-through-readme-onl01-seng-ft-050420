5a69f7414c0c4690b14c03e88926edfa1fb31c66
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-14 02:00:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,7 +18,9 @@ class Waiter
   end 
   
   def meals
-    Meal.all.select {|meal| meal.waiter == self}
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+    end
   end 
     
   def best_tipper
