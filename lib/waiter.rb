fcc9eb1b1a508afc9855fdae40c22c8c4c97473d
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-14 01:57:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,40 +1,49 @@
-class Waiter
-  attr_accessor :name, :yrs_experience
-  
+class Customer
+  attr_accessor :name, :age
+ 
   @@all = []
-  
-  def initialize(name, yrs_experience)
-    @name = name 
-    @yrs_experience = yrs_experience
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
     @@all << self
   end
-  
+ 
   def self.all
     @@all
-  end 
-  
-  def new_meal
-    Meal.new(self, customer, total, tip)
-  end 
-  
+  end
+ 
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+ 
   def waiters
     meals.map do |meal|
       meal.waiter
     end
   end
-    
-  def meals
-    Meal.all.select do |meal|
-      meal.customer == self
-    end
-  end 
-      
-  def best_tipper
-    best_tipped_meal = meals.max do |meal_a, meal_b|
-      meal_a.tip <=> meal_b.tip
-    end
-   
-    best_tipped_meal.customer
+ 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
   end
-  
+ 
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
+  end
+ 
 end
